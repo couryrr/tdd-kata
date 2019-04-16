f113d733d605fb3772625a6dc0090fd97b628873
Couyrr <coury.richards@gmail.com>
2019-04-16 09:16:38 -0400

    Test coverage upped

--- src/main/java/com/dappercloud/StringCalculator.java
+++ src/main/java/com/dappercloud/StringCalculator.java
@@ -1,25 +1,29 @@
 package com.dappercloud;
 
 public class StringCalculator {
-	
+
 	/**
 	 * @param input - String separated by commas
 	 * @return value of the separated string.
 	 * 
-	 * The method can take up to two numbers, separated by commas, and will return their sum. 
-	 * for example “” or “1” or “1,2” as inputs.
-	 * (for an empty string it will return 0) 
+	 *         The method can take up to two numbers, separated by commas, and will
+	 *         return their sum. for example “” or “1” or “1,2” as inputs. (for an
+	 *         empty string it will return 0)
 	 */
 	public static int addString(String input) {
-		
-			if(input.contains(",")) {
-				//TODO: Split the input, parse ints and add
-				return 7;
-			}
-			
-			return parse(input);
+
+		int result = 0;
+		for (String value : split(input)) {
+			result += parse(value);
+		}
+
+		return result;
 	}
-	
+
+	public static String[] split(String input) {
+		return input.split(",");
+	}
+
 	public static int parse(String value) {
 		try {
 			return Integer.parseInt(value);
--- src/test/java/com/dappercloud/StringCalculatorTest.java
+++ src/test/java/com/dappercloud/StringCalculatorTest.java
@@ -33,6 +33,17 @@ public class StringCalculatorTest {
 	}
 	
 	@Test
+	public void addTwoFirstEmptyStringTest() {
+		int result = StringCalculator.addString(",2");
+		assertEquals("String input ,2 is 2", 2, result);
+	}
+	@Test
+	public void addTwoSecondEmptyStringTest() {
+		int result = StringCalculator.addString("5,");
+		assertEquals("String input 5, is 5", 5, result);
+	}
+	
+	@Test
 	public void parseEmptyTest() {
 		int result = StringCalculator.parse("");
 		assertEquals("String input blank is 0", 0, result);
@@ -43,4 +54,54 @@ public class StringCalculatorTest {
 		int result = StringCalculator.parse(" ");
 		assertEquals("String input white space is 0", 0, result);
 	}
+	
+	@Test
+	public void parseNumberTest() {
+		int result = StringCalculator.parse("5");
+		assertEquals("String input 5 is 5", 5, result);
+	}
+	
+	@Test
+	public void parseNegativeNumberTest() {
+		int result = StringCalculator.parse("-5");
+		assertEquals("String input -5 is -5", -5, result);
+	}
+	
+	@Test
+	public void splitEmptyStringTest() {
+		String[] result = StringCalculator.split("");
+		assertEquals("Empty input will have a size of 1", 1, result.length);
+		assertEquals("Empty input only value will be blank", "", result[0]);
+	}
+	
+	@Test
+	public void splitSingleStringTest() {
+		String[] result = StringCalculator.split("5");
+		assertEquals("Single input will have a size of 1", 1, result.length);
+		assertEquals("Single input only value will be it", "5", result[0]);
+	}
+	
+	@Test
+	public void splitTwoStringTest() {
+		String[] result = StringCalculator.split("5,2");
+		assertEquals("Two inputs will have a size of 2", 2, result.length);
+		assertEquals("Input[0] value will be it", "5", result[0]);
+		assertEquals("Input[1] value will be it", "2", result[1]);
+	}
+	
+	@Test
+	public void splitTwoOneBlankStringTest() {
+		String[] result = StringCalculator.split("5, ");
+		assertEquals("Two inputs will have a size of 2", 2, result.length);
+		assertEquals("Input[0] value will be it", "5", result[0]);
+		assertEquals("Input[1] value will be it", " ", result[1]);
+	}
+	
+	@Test
+	public void splitTwoOneEmptyStringTest() {
+		String[] result = StringCalculator.split("25,");
+		assertEquals("Two inputs will have a size of 1", 1, result.length);
+		assertEquals("Input[0] value will be it", "25", result[0]);
+	}
+
 }
